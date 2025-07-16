5872b6f648bd050c654cd6dc6850ab1aeba49eda
imujinu <wlsdnrhdwkd@gmail.com>
2025-07-16 12:49:12 +0900

    jpa

---
+++ src/main/java/com/beyond/basic/b2_board/repository/AuthorJpaRepository.java
@@ -0,0 +1,33 @@
+//package com.beyond.basic.b2_board.repository;
+//
+//import com.beyond.basic.b2_board.domain.Author;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Repository
+//public class AuthorJpaRepository {
+//
+//    public void save(Author author){
+//    }
+//
+//    public List<Author> findAll(){
+//        return null;
+//    }
+//
+//    public Optional<Author> findById(Long id){
+//        return null;
+//
+//
+//    }
+//
+//    public Optional<Author> findByEmail(String email){
+//        return null;
+//    }
+//
+//    public void delete(Long id){
+//
+//    }
+//
+//}
